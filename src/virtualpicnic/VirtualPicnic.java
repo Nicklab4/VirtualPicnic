187e40a813e22fed00ff3d7dbe3529bfc3953c76
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-03-11 13:20:06 +0500

    Virtual Picnic v.02

--- src/virtualpicnic/VirtualPicnic.java
+++ src/virtualpicnic/VirtualPicnic.java
@@ -19,17 +19,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class VirtualPicnic {
     public static void main(String[] args) {
         String filename = "input.txt";
         List<String> ls = new ArrayList<String>();
 
+        openFile(filename, ls);
+
+        System.out.println("Количество слов в файле - " + filename + " - " + ls.size());
+
+        checkLongestWord(ls);
+
+        statisticM(ls);
+
+
+    }
+
+    public static void openFile(String fileName, List<String> ls){
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
             String string = reader.readLine();
             while (string != null){
                 Collections.addAll(ls, string.split(" "));
@@ -43,8 +53,48 @@ public class VirtualPicnic {
             throw new RuntimeException(e);
         }
 
-        System.out.println(ls.getLast());
-
-
+        ls.removeIf(String::isEmpty);
     }
+
+    public static void checkLongestWord(List<String> ls)
+    {
+        int temp = 0;
+        String value = "";
+        for(String str : ls){
+            if(str.length() > temp) {
+                temp = str.length();
+                value = str;
+            }
+        }
+
+        System.out.println("Самое длинное слово - " + value + ", " + temp + " символов");
+    }
+
+    public static void statisticM(List<String> ls){
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for (String i : ls  ){
+            if(map.containsKey(i))
+                map.put(i, map.get(i) + 1);
+            else map.put(i,  1);
+        }
+
+        System.out.println(map);
+        String[] values = new String[map.size()];
+        //Set<String> mapValues =
+        map.keySet().toArray(values);
+
+        int maxVeget = 0;
+        String maxVegetKey = values[0];
+        for (int i = 0; i < values.length; i++) {
+            if (maxVeget < map.get(values[i])) {
+                maxVeget = map.get(values[i]);
+                maxVegetKey = values[i];
+            }
+        }
+
+        System.out.println("Самый популярный фрукт/овощ - " + maxVegetKey +
+                " - " + map.get(maxVegetKey) + " значений.");
+    }
+
 }
